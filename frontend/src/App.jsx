429715e7459384a25dfa65ca06cfb8fe2c6688d8
minchenlee <110967458+minchenlee@users.noreply.github.com>
2023-11-06 21:39:11 +0800

    Feat: Frontend Initial configuration & routing
    - Configured Tailwind styles for the frontend
    - Established routing functionality for the application
    - Added font, icon, and image files to the project.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// fonts import
+import "@fontsource/roboto-mono"; // Defaults to weight 400
+import "@fontsource/roboto-mono/600.css"
+
+import { useState, useContext, useEffect } from "react"
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+
+import "./App.css"
+import "bootstrap-icons/font/bootstrap-icons.css";
+import JoinContext from "./contexts/JoinContext"
+import Layout from "./page/Layout"
+import ScrollToTop from "./utilities/ScrollToTop";
+import ErrorPage from "./page/Error";
+import BookingPage from "./page/Booking"
+import BookingDetailPage from "./page/BookingDetail";
+import MateFindingPage from "./page/MateFinding"
+import JoiningPage from "./page/Joining"
+import JoiningDetailPage from "./page/JoiningDetail";
+import RecordsPage from "./page/Records";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "", element: <BookingPage />},
+      { path: "booking", element: <BookingPage />},
+      { path: "findmate", element: <MateFindingPage /> },
+      { path: "findmate/join", element: <JoiningPage /> },
+      { path: "findmate/join/:day", element: <JoiningDetailPage /> },
+      { path: "booking/:sport", element: <BookingDetailPage /> },
+      { path: "records", element: <RecordsPage /> },
+    ],
+  }
+]);
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 選擇日期、選擇運動
+  const [selectedDayCode, setSelectedDayCode] = useState(0);
+  const [selectedSport, setSelectedSport] = useState("basketball");
+
+  // Modal 狀態追蹤
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Join Detail view
+  const [selectedJoinId, setSelectedJoinId] = useState();
+  
+  // Join Context
+  const joinContextValue = {
+    selectedDayCode, setSelectedDayCode,
+    selectedSport, setSelectedSport,
+    isModalOpen, setIsModalOpen,
+    selectedJoinId, setSelectedJoinId,
+  }
+
+  useEffect(() => {
+    console.log("selectedDayCode: ", selectedDayCode);
+    console.log("selectedSport: ", selectedSport);
+  }, [selectedDayCode, selectedSport])
+
+  useEffect(() => {
+    console.log("selectedJoinId: ", selectedJoinId);
+
+    if (selectedJoinId === undefined) return;
+    
+    const data = JSON.parse(window.localStorage.getItem("joinDetailJson"));
+    console.log(data.find(item => item.id === selectedJoinId));
+  }, [selectedJoinId])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <JoinContext.Provider value={joinContextValue}>
+          <RouterProvider router={router}/>
+      </JoinContext.Provider>
+    </LocalizationProvider>
   )
 }
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,58 @@
+/** @type {import("tailwindcss").Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  // important: '#root',
+  theme: {
+    fontFamily: {
+      sansTC: ["Noto Sans TC"],
+      robotoMono: ["Roboto Mono"]
+    },
+    fontSize: {
+      sm: "0.75rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+    },
+    extend: {
+      colors: {
+        "primary": "#292929",
+        "white": "#FFFFFF",
+        "black": "#000000",
+        "light-silver": "#EEEEEE",
+        "silver": "#CCCCCC",
+        "gray": "#858585",
+        "dark-gray": "#3F3F3F",
+        "light-green": "#D3E59F",
+        "peach": "#F4B092",
+        "fade-blue":"#4D9F9E",
+      },
+      backgroundImage: {
+        "basketball-card": "url(../src/assets/image/basketball_bg.jpg)",
+        "badminton-card": "url(../src/assets/image/badminton_bg.jpg)",
+        "volleyball-card": "url(../src/assets/image/volleyball_bg.jpg)",
+        "tennis-card": "url(../src/assets/image/tennis_bg.jpg)",
+        "table-tennis-card": "url(../src/assets/image/table_tennis_bg.jpg)",
+        "court-recruit-card": "url(../src/assets/image/court_recruit_bg.jpg)",
+        "court-join-card": "url(../src/assets/image/court_join_bg.jpg)",
+      },
+      backgroundSize: {
+        '110': '110%',
+      },
+      backdropContrast: {
+        110: "1.1",
+      },
+      scale: {
+        '101': '1.01',
+      },
+      borderWidth: {
+        '1': '1px',
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar')({ nocompatible: true }),
+  ],
+}
